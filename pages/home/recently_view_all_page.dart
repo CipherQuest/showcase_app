b8e03eebcd2dcf17c9a0a8509b13db865ac8e5e6
CipherQuest <careers@thebeelogix.com>
2024-02-27 09:06:46 +0000

    Add comment to pages/home/recently_view_all_page.dart in folder pages/home

--- pages/home/recently_view_all_page.dart
+++ pages/home/recently_view_all_page.dart
@@ -108,3 +108,4 @@ class RecentlyViewAllPage extends GetView<RecentlyViewAllPageController> {
 # Contribution: Added comment
 # Contribution: Added comment
 # Contribution: Added comment
+# Contribution: Added comment
