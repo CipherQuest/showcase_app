80fc71952f391dd40052cc5808502f11d9842a98
CipherQuest <careers@thebeelogix.com>
2024-08-16 18:11:02 +0000

    Add comment to pages/home/home_page.dart in folder pages

--- pages/home/home_page.dart
+++ pages/home/home_page.dart
@@ -785,3 +785,4 @@ class HomePage extends GetView<HomeController> with CompanyWidgetsMixin {
 # Contribution: Added comment
 # Contribution: Added comment
 # Contribution: Added comment
+# Contribution: Added comment
