6f3a5aef0f1d33210df7ec834c1cf2de2c77e8c2
CipherQuest <careers@thebeelogix.com>
2024-10-09 12:16:20 +0000

    Add comment to pages/login/forgotten_password_page.dart in folder pages/login

--- pages/login/forgotten_password_page.dart
+++ pages/login/forgotten_password_page.dart
@@ -408,3 +408,4 @@ class ForgottenPasswordPage extends GetView<LoginController> {
 # Contribution: Added comment
 # Contribution: Added comment
 # Contribution: Added comment
+# Contribution: Added comment
