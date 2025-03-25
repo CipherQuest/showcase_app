4437bc5a0245d1e833bca2589956927381ca4b94
CipherQuest <careers@thebeelogix.com>
2025-03-25 06:14:13 +0000

    Add comment to pages/notifications/notification_page.dart in folder pages/notifications

--- pages/notifications/notification_page.dart
+++ pages/notifications/notification_page.dart
@@ -1073,3 +1073,4 @@ class NotificationsPage extends GetView<NotificationController> {
 # Contribution: Added comment
 # Contribution: Added comment
 # Contribution: Added comment
+# Contribution: Added comment
