2715f3e8e71de852bc6f19e9101cc38d8f3c0f63
CipherQuest <careers@thebeelogix.com>
2026-03-17 04:44:24 +0000

    Add comment to pages/property_listing/property_create_page.dart in folder pages

--- pages/property_listing/property_create_page.dart
+++ pages/property_listing/property_create_page.dart
@@ -540,3 +540,4 @@ class PropertyCreatePage extends GetView<PropertyCreateController>
 # Contribution: Added comment
 # Contribution: Added comment
 # Contribution: Added comment
+# Contribution: Added comment
